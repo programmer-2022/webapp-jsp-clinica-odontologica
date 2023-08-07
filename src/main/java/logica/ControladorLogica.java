4d0452392467b7d11cd1bc22cd944a673b168344
Cesar Martinez <km.music92@gmail.com>
2023-08-06 20:11:19 -0500

    update: obtenerUsuarios function

--- src/main/java/logica/ControladorLogica.java
+++ src/main/java/logica/ControladorLogica.java
@@ -1,13 +1,21 @@
 package logica;
 
+import java.util.List;
 import persistencia.ControladorPersistencia;
 
 public class ControladorLogica {
     
     ControladorPersistencia controladoraPersistencia = new ControladorPersistencia();
     
-    public void crearUsuario(int id, String nombre, String clave, String rol) {
-        Usuario usu = new Usuario(id, nombre, clave, rol);
-        controladoraPersistencia.crearUsuario(usu);
+    public void crearUsuario(String nombre, String contrasena, String rol) {
+        Usuario usuario = new Usuario();
+        usuario.setNombre(nombre);
+        usuario.setClave(contrasena);
+        usuario.setRol(rol);
+        controladoraPersistencia.crearUsuario(usuario);
+    }
+    
+    public List<Usuario> obtenerUsuarios() {
+        return controladoraPersistencia.obtenerUsuarios();
     }
 }
--- src/main/java/persistencia/ControladorPersistencia.java
+++ src/main/java/persistencia/ControladorPersistencia.java
@@ -1,22 +1,36 @@
 package persistencia;
 
+import java.util.List;
 import logica.Usuario;
 
 public class ControladorPersistencia {
-    
-    HorarioJpaController horarioJpaControllerJPA = new HorarioJpaController();
-    OdontologoJpaController odontologoJpaControllerJPA = new OdontologoJpaController();
-    PacienteJpaController pacienteJpaControllerJPA = new PacienteJpaController();
-    PersonaJpaController personaJpaControllerJPA = new PersonaJpaController();
-    ResponsableJpaController responsableJpaControllerJPA = new ResponsableJpaController();
-    SecretarioJpaController secretarioJpaControllerJPA = new SecretarioJpaController();
-    TurnoJpaController turnoJpaControllerJPA = new TurnoJpaController();
-    UsuarioJpaController usuarioJpaControllerJPA = new UsuarioJpaController();
 
-    public ControladorPersistencia() {   }
-    
-     public void crearUsuario(Usuario usuario) {
-         usuarioJpaControllerJPA.create(usuario);
+    HorarioJpaController horarioJpaControllerJPA;
+    OdontologoJpaController odontologoJpaControllerJPA;
+    PacienteJpaController pacienteJpaControllerJPA;
+    PersonaJpaController personaJpaControllerJPA;
+    ResponsableJpaController responsableJpaControllerJPA;
+    SecretarioJpaController secretarioJpaControllerJPA;
+    TurnoJpaController turnoJpaControllerJPA;
+    UsuarioJpaController usuarioJpaControllerJPA;
+
+    public ControladorPersistencia() {
+        horarioJpaControllerJPA = new HorarioJpaController();
+        odontologoJpaControllerJPA = new OdontologoJpaController();
+        pacienteJpaControllerJPA = new PacienteJpaController();
+        personaJpaControllerJPA = new PersonaJpaController();
+        responsableJpaControllerJPA = new ResponsableJpaController();
+        secretarioJpaControllerJPA = new SecretarioJpaController();
+        turnoJpaControllerJPA = new TurnoJpaController();
+        usuarioJpaControllerJPA = new UsuarioJpaController();
     }
-    
+
+    public void crearUsuario(Usuario usuario) {
+        usuarioJpaControllerJPA.create(usuario);
+    }
+
+    public List<Usuario> obtenerUsuarios() {
+        return usuarioJpaControllerJPA.findUsuarioEntities();
+    }
+
 }
